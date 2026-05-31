fd8a821074d88bf7d33d3dea0e55ab6fcc043f54
Ifad Yusuf <86540168+Dhlih@users.noreply.github.com>
2026-05-31 12:25:09 +0700

    Refactor MLflow run context for model training

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
+mlflow.set_tracking_uri("file:./mlruns")
 mlflow.set_experiment(experiment_name="anime_score_prediction")
 
 df = pd.read_csv("anime_preprocessing.csv")
@@ -16,14 +17,13 @@ model = RandomForestRegressor(n_estimators=100, random_state=42, max_depth=20, m
 
 mlflow.sklearn.autolog()
 
-with mlflow.start_run():
-    model.fit(X_train, y_train)
+model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
-    mae = mean_absolute_error(y_test, y_pred)
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
+y_pred = model.predict(X_test)
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
 
-    print(f"MAE: {mae}")
-    print(f"MSE: {mse}")
-    print(f"R2 Score: {r2}")
+print(f"MAE: {mae}")
+print(f"MSE: {mse}")
+print(f"R2 Score: {r2}")
