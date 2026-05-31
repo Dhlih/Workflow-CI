3b52b160bd4a3ca0306c56580eec00f522ebf0cd
Ifad Yusuf <86540168+Dhlih@users.noreply.github.com>
2026-05-31 12:40:09 +0700

    Update RandomForest model training and logging
    Refactor model training and logging metrics with MLflow.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,29 +1,39 @@
 import mlflow
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 mlflow.set_tracking_uri("file:./mlruns")
-mlflow.set_experiment(experiment_name="anime_score_prediction")
+mlflow.set_experiment("anime_score_prediction")
 
 df = pd.read_csv("anime_preprocessing.csv")
 
 X = df.drop(columns=["score"])
 y = df["score"]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = RandomForestRegressor(n_estimators=100, random_state=42, max_depth=20, min_samples_split=5, min_samples_leaf=2)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
-mlflow.sklearn.autolog()
+model = RandomForestRegressor(
+    n_estimators=100,
+    max_depth=20,
+    min_samples_split=5,
+    min_samples_leaf=2,
+    random_state=42
+)
 
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
-mae = mean_absolute_error(y_test, y_pred)
+
 mse = mean_squared_error(y_test, y_pred)
+mae = mean_absolute_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
-print(f"MAE: {mae}")
-print(f"MSE: {mse}")
-print(f"R2 Score: {r2}")
+mlflow.log_metric("mse", mse)
+mlflow.log_metric("mae", mae)
+mlflow.log_metric("r2", r2)
+
+mlflow.sklearn.log_model(model, "model")
